c78120f75b8802b683d8aa4470ea45af1665e5dc
roy-pstr <roy.pasternakk@gmail.com>
2022-11-20 22:51:04 +0200

    add tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,8 +2,25 @@ import pytest
 from fastapi.testclient import TestClient
 
 from src.main import app
+from src.config import get_settings
 
 @pytest.fixture(scope="session")
 def client():
     with TestClient(app) as test_client:
         yield test_client
+
+@pytest.fixture(scope="session")
+def headers_with_authorization(client: TestClient):
+    settings = get_settings()
+    response = client.post(
+        "/token",
+        data={"username": settings.AUTH0_TEST_USERNAME, "password": settings.AUTH0_TEST_PASSWORD},
+    )
+    response.raise_for_status()
+    access_token = response.json().get("access_token")
+    return {"Authorization": f"Bearer {access_token}"}
+
+# @pytest.fixture(scope="session")
+# def test_client_auth(headers_with_authorization: dict):
+#     with TestClient(app, headers=headers_with_authorization) as test_client:
+#         yield test_client
---
+++ backend/tests/test_private.py
@@ -0,0 +1,7 @@
+from fastapi.testclient import TestClient
+
+def test_me(client: TestClient, headers_with_authorization: dict):
+    response = client.get("/private", headers=headers_with_authorization)
+    if response.status_code != 200:
+        print(response.text)
+    assert response.status_code == 200
