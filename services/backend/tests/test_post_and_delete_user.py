7d89eb0f0203751317f42b98e33c572b0d280937
roy-pstr <roy.pasternakk@gmail.com>
2022-12-07 15:21:04 +0200

    add test for weak password and unvalid username (email)

--- services/backend/tests/test_post_and_delete_user.py
+++ services/backend/tests/test_post_and_delete_user.py
@@ -12,19 +12,41 @@ mark_parametrize = [
             "given_name": "string",
             "family_name": "string",
             "name": "string",
-        }
+        },
+        200
+    ),
+    (
+        {
+            "email": f"user_{str(uuid4())[-8:]}@example.com",
+            "password": "this_is_weak_password",
+            "given_name": "string",
+            "family_name": "string",
+            "name": "string",
+        },
+        400
+    ),
+    (
+        {
+            "email": f"unvalid_email_address",
+            "password": "secret234!@#$!@",
+            "given_name": "string",
+            "family_name": "string",
+            "name": "string",
+        },
+        422
     ),
 ]
-@pytest.mark.parametrize("user_data", mark_parametrize)
-def test_user_signup_and_remove(user_data: dict, test_client_auth: TestClient):
+@pytest.mark.parametrize("user_data,expected_status_code", mark_parametrize)
+def test_user_signup_and_remove(user_data: dict, expected_status_code: int, test_client_auth: TestClient):
     response = test_client_auth.post("/user", json=user_data)
-    if response.status_code != 200:
+    if response.status_code != expected_status_code:
         print(response.text)
-    response.raise_for_status()
-    user_id = response.json()["user_id"]
-    assert user_id != None
-    time.sleep(2) # it takes time for the roles to be updated after create user in auth0.
-    response = test_client_auth.delete(f"/user/{user_id}")
-    if response.status_code != 200:
-        print(response.text)
-    assert response.status_code == 200
+    if expected_status_code == 200:            
+        response.raise_for_status()
+        user_id = response.json()["user_id"]
+        assert user_id != None
+        time.sleep(2) # it takes time for the roles to be updated after create user in auth0.
+        response = test_client_auth.delete(f"/user/{user_id}")
+        if response.status_code != 200:
+            print(response.text)
+        assert response.status_code == 200
